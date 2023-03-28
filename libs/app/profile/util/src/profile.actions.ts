d804aafca51d32d08857724594ad992c079d09d8
Jake <jake.mileham@gmail.com>
2023-03-28 06:17:52 +0000

    Updated Profile State and Api

--- libs/app/profile/util/src/profile.actions.ts
+++ libs/app/profile/util/src/profile.actions.ts
@@ -1,4 +1,4 @@
-import { IProfile } from '@mp/api/profiles/util';
+import { user_profile } from '@mp/api/profiles/util';
 
 export class Logout {
   static readonly type = '[Profile] Logout';
@@ -10,7 +10,7 @@ export class SubscribeToProfile {
 
 export class SetProfile {
   static readonly type = '[Profile] SetProfile';
-  constructor(public readonly profile: IProfile | null) {}
+  constructor(public readonly profile: user_profile | null) { }
 }
 
 export class UpdateAccountDetails {
