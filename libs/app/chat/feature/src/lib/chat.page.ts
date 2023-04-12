ecc50c618ed43b85153e7ec7be3c11d1c52ca136
UnarineLeo <unarineleo@gmail.com>
2023-04-12 10:41:30 +0200

    chat page - almost final page

--- libs/app/chat/feature/src/lib/chat.page.ts
+++ libs/app/chat/feature/src/lib/chat.page.ts
@@ -2,11 +2,12 @@
 import { Component } from '@angular/core';
 // import { NavController } from '@ionic/angular/providers/nav-controller';
 
-// interface ChatMessage {
-//   message: string;
-//   sender: string;
-//   timestamp: number;
-// }
+interface ChatMessage {
+  message: string;
+  sender: string;
+  timestamp: number;
+  
+}
 
 @Component({
   selector: 'chat-page',
@@ -14,21 +15,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat.page.css']
 })
 export class ChatPage {
-  // constructor() {}
-  // messages: ChatMessage[] = [
-  //   { message: 'Hello', sender: 'user', timestamp: 123456789 },
-  //   { message: 'Hi', sender: 'bot', timestamp: 123456789 },
-  //   { message: 'How are you?', sender: 'user', timestamp: 123456789 },
-  //   { message: 'I am fine', sender: 'bot', timestamp: 123456789 },
-  //   { message: 'What about you?', sender: 'user', timestamp: 123456789 },
-  //   { message: 'I am fine too', sender: 'bot', timestamp: 123456789 },
-  //   { message: 'What do you do?', sender: 'user', timestamp: 123456789 },
-  //   { message: 'I am a bot', sender: 'bot', timestamp: 123456789 },
-  // ]
-  // sendMessage(message: string) {
-  //   this.messages.push({ message, sender: 'user', timestamp: Date.now() });
-  // }
-  
 
+  messages: ChatMessage[] = [
+    { message: 'Hello', sender: 'user', timestamp: 123456789 },
+    { message: 'Hi', sender: 'bot', timestamp: 123456789 },
+    { message: 'How are you?', sender: 'user', timestamp: 123456789 },
+    { message: 'I am fine', sender: 'bot', timestamp: 123456789 },
+    { message: 'What about you?', sender: 'user', timestamp: 123456789 },
+    { message: 'I am fine too', sender: 'bot', timestamp: 123456789 },
+    { message: 'What do you do?', sender: 'user', timestamp: 123456789 },
+    { message: 'I am a bot', sender: 'bot', timestamp: 123456789 },
+  ]
+  sendMessage(message: string) {
+    this.messages.push({ message, sender: 'user', timestamp: Date.now() });
+  }
 
+  newMessage = '';
 }
