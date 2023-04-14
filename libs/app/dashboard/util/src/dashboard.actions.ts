988d4b41163ee75f0125d8b7ff2ea1c7b26130f9
Lukas Anthonissen <lukasanthonissen@gmail.com>
2023-04-14 19:42:08 +0200

    Remove SetPosts action constructor

--- libs/app/dashboard/util/src/dashboard.actions.ts
+++ libs/app/dashboard/util/src/dashboard.actions.ts
@@ -1,6 +1,3 @@
-import { user_profile } from "@mp/api/profiles/util";
-
 export class SetPosts {
     static readonly type = '[Dashboard] SetPosts';
-    constructor(public readonly user: user_profile | null) {}
 }
