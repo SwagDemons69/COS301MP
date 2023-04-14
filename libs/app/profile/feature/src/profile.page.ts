3146f39608d8d2c4a3fed3f43dce6585c0c49102
Rec1dite <rec1dite@gmail.com>
2023-04-14 08:27:31 +0000

    Added basic profile edit page

--- libs/app/profile/feature/src/profile.page.ts
+++ libs/app/profile/feature/src/profile.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { user_profile } from '@mp/api/profiles/util';
-import { ModalController } from '@ionic/angular';
+import { AlertController, ModalController } from '@ionic/angular';
 import { BlipComponent } from '@mp/app//shared-components';
 import { ProfileState } from '@mp/app/profile/data-access';
 import { Select } from '@ngxs/store';
@@ -10,15 +11,29 @@ import { post } from '@mp/api/home/util';
 @Component({
   selector: 'ms-profile-page',
   templateUrl: './profile.page.html',
-  styleUrls: ['./profile.page.scss'],
+  styleUrls: ['./profile.page.scss']
 })
 export class ProfilePage {
   @Select(ProfileState.profile) profile$!: Observable<user_profile | null>;
   @Select(ProfileState.posts) posts$!: Observable<post | null>;
+  userForm: FormGroup;
 
   constructor(
     private modalController: ModalController,
-  ) {}
+    private formBuilder: FormBuilder
+  )
+  {
+    this.userForm = this.formBuilder.group({
+      notPublic: ['', Validators.required],
+      name: ['', Validators.required],
+      username: ['', Validators.required],
+      profilePicturePath: [''],
+      bio: ['', Validators.required],
+      province: ['', Validators.required]
+    });
+  }
+
+  isEditingProfile = false;
 
   // data at the moment expects a json object as follows:
   // {title: "...", desc: "...", img: "..."}
@@ -39,4 +54,10 @@ export class ProfilePage {
 
     return await modal.present();
   }
+
+  updateProfile() {
+    alert(JSON.stringify(this.userForm.value, null, 2));
+    this.isEditingProfile = false;
+  }
+
 }
