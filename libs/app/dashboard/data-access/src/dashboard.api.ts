c5596fbffb0653d7fc7338d223b97892384ea26b
Lukas Anthonissen <lukasanthonissen@gmail.com>
2023-04-14 19:43:29 +0200

    Add posts api request

--- libs/app/dashboard/data-access/src/dashboard.api.ts
+++ libs/app/dashboard/data-access/src/dashboard.api.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import { doc, docData, Firestore } from '@angular/fire/firestore';
-import { Functions, httpsCallable } from '@angular/fire/functions';
+import { Functions } from '@angular/fire/functions';
 import { post } from '@mp/api/home/util';
+import { map, Observable } from 'rxjs';
+import { from } from 'rxjs';  
+import 'firebase/compat/database';
+import 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';@Injectable()
 
-@Injectable()
+
 export class DashboardApi {
   constructor(
     private readonly firestore: Firestore,
@@ -11,6 +16,8 @@ export class DashboardApi {
   ) {}
 
   post$(id: string) {
+    
+
     const docRef = doc(
       this.firestore,
       `posts/${id}`
@@ -22,4 +29,26 @@ export class DashboardApi {
     });
     return docData(docRef, { idField: 'id' });
   }
+
+  posts$(): Observable<post[]> {
+    const postsRef = collection(this.firestore, 'posts');
+    return from(getDocs(postsRef)).pipe(
+      map((querySnapshot) => querySnapshot.docs.map((doc) => ({
+        post_id : doc.id,
+        user_id : doc.data()['user_id'],
+        content : doc.data()['content'],
+        caption : doc.data()['caption'],
+        likes : doc.data()['likes'],
+        timeStamp : doc.data()['timeStamp'],
+        shares : doc.data()['shares'],
+        kronos : doc.data()['kronos'],
+        comments : doc.data()['comments'],
+        categories : doc.data()['categories'],
+        taggedUsers : doc.data()['taggedUsers']
+      })))
+    );
+  }
 }
+
+
+
