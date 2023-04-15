7c9e1e546f8b2b4d068e9bc81906107ed342aba9
ConradStras <cadstrasheim@gmail.com>
2023-04-15 12:18:11 +0000

    small typo fix

--- libs/api/messages/util/src/interfaces/contacts.interface.ts
+++ libs/api/messages/util/src/interfaces/contacts.interface.ts
@@ -2,5 +2,5 @@ import { IContact } from "./contact.interface";
 
 export interface IContacts{
     following: IContact [] | null | undefined;
-    follwoers: IContact [] | null | undefined;
+    followers: IContact [] | null | undefined;
 }
