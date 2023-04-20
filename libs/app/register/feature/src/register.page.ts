debb64cc10284059ac46366977ba29aa26f5f6a9
Mduduzi Sibiya <mdundoro@gmail.com>
2023-04-20 14:01:51 +0200

    username and confirm_password

--- libs/app/register/feature/src/register.page.ts
+++ libs/app/register/feature/src/register.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Register } from '@mp/app/register/util';
 import {
     ActionsExecuting,
@@ -21,9 +21,30 @@ export class RegisterPage {
       '',
       [Validators.email, Validators.minLength(6), Validators.maxLength(64)],
     ],
+    username: ['', [Validators.minLength(3), Validators.maxLength(64)]],
     password: ['', [Validators.minLength(6), Validators.maxLength(64)]],
+    confirmPassword: ['', [Validators.minLength(6), Validators.maxLength(64)]],
   });
+
+  // matchValues(matchTo: string) {
+  //   return (formGroup: FormGroup) => {
+  //     const control = formGroup.controls[matchTo];
+  //     const matchingControl = formGroup.controls['confirmPassword'];
+
+  //     if (control.value !== matchingControl.value) {
+  //       matchingControl.setErrors({ passwordMismatch: true });
+  //     } else {
+  //       matchingControl.setErrors(null);
+  //     }
+  //   };
+  // }
+
   showPassword = false;
+ 
+
+  get username() {
+    return this.registerForm.get('username');
+  }
 
   get email() {
     return this.registerForm.get('email');
@@ -32,7 +53,19 @@ export class RegisterPage {
   get password() {
     return this.registerForm.get('password');
   }
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword');
+  }
+  get usernameError(): string {
+    if (this.username?.errors?.['username']) return 'username is invalid';
+    if (this.username?.errors?.['required']) return 'username is required';
+    if (this.username?.errors?.['minlength'])
+      return 'username should be longer than 6 characters';
+    if (this.username?.errors?.['maxlength'])
+      return 'username should be shorter than 64 characters';
 
+    return 'Email is invalid';
+  }
   get emailError(): string {
     if (this.email?.errors?.['email']) return 'Email is invalid';
     if (this.email?.errors?.['required']) return 'Email is required';
@@ -53,6 +86,11 @@ export class RegisterPage {
 
     return 'Password is invalid';
   }
+  get confirmPasswordError(): string {  
+    if (this.password?.value !== this.confirmPassword?.value) return 'Passwords do not match';
+  
+    return 'Confirm password is invalid';
+  }
 
   constructor(
     private readonly fb: FormBuilder,
@@ -60,7 +98,7 @@ export class RegisterPage {
   ) {}
 
   register() {
-    if (this.registerForm.valid) {
+    if (this.registerForm.valid && this.password?.value === this.confirmPassword?.value) {
       this.store.dispatch(new Register());
     }
   }
