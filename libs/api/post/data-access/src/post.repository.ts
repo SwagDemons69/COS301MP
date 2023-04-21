b4c094d39c4c3a27940adbf1cc3bc0791d3de6b5
Jake <jake.mileham@gmail.com>
2023-04-21 08:24:03 +0000

    Minor fixes in post repository.

--- libs/api/post/data-access/src/post.repository.ts
+++ libs/api/post/data-access/src/post.repository.ts
@@ -85,23 +85,3 @@ export class PostRepository {
         return { posts: handle.docs.map((doc) => { return doc.data() as post; })};
     }
 }
-
-    // const handle = await db.collection('profiles').doc(user);
-   
-    // const post = await Promise.resolve(handle.get());
-    // console.log(post.data()['posts'][postId]) 
-
-// async blobToDataURL(blob: Blob): Promise<string> {
-//     return new Promise<string>((resolve, reject) => {
-//       const reader = new FileReader();
-//       reader.onload = () => resolve(reader.result as string);
-//       reader.onerror = reject;
-//       reader.readAsDataURL(blob);
-//     });
-
-// return uploadString(photosRef, file, 'base64').then((snapshot) => { 
-//     console.log('Uploaded a base64 string!');
-//     console.log(typeof snapshot.ref);
-//     const JSONFORM : JSON = JSON.parse(JSON.stringify(snapshot.ref, null, 2));
-//     console.log(snapshot.metadata.fullPath)
-// });
