cc4a9da1d0e66a66a70acdc1d7561c223ca81b0b
Emito Khoza <emito.khoza@gmail.com>
2023-04-22 16:18:43 +0000

    Updated notification repository

--- libs/api/notifications/data-access/src/notifications.repository.ts
+++ libs/api/notifications/data-access/src/notifications.repository.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { getStorage, ref , uploadBytes, connectStorageEmulator, uploadString} from 'firebase/storage';
 import { initializeApp } from '@firebase/app';
-import { GetNotificationsResponse } from '@mp/api/util';
-import { SendNotificationRequest, SendNotificationResponse } from '@mp/api/util';
-import { notification } from '@mp/api/util';
+import { GetNotificationsResponse } from '@mp/api/notifications/util';
+import { SendNotificationRequest, SendNotificationReponse } from '@mp/api/notifications/util';
+import { notification } from '@mp/api/notifications/util';
 import * as admin from 'firebase-admin';
 // import { IProfile } from '@mp/api/profiles/util';
 // import { post } from '@mp/api/post/util';
@@ -11,14 +11,14 @@ import * as admin from 'firebase-admin';
 @Injectable()
 export class NotificationRepository {
 
-    async sendNotification(sendNotificationRequest : SendNotificationRequest): Promise<SendNotificationResponse> {
+    async sendNotification(sendNotificationRequest : SendNotificationRequest): Promise<any> {
         // create a notification object and add it to the firebase collection for notifications
         // create a document in the firebase collection for notifications in user collection
         // add the notification id to the user's notifications array
         
     }
     
-    async getNotifications(user_id: string): Promise<GetNotificationsResponse> {
+    async getNotifications(user_id: string): Promise<any> {
         // get an array of notifications for a user from the firebase collection for notifications
     }
 }
