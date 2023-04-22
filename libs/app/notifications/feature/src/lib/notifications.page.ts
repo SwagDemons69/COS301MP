19959d34224861c850874bbb283211b6b7bb7bce
Tumi Pare <u21452832@tuks.co.za>
2023-04-22 20:59:22 +0200

    Updated Notification FrontEnd

--- libs/app/notifications/feature/src/lib/notifications.page.ts
+++ libs/app/notifications/feature/src/lib/notifications.page.ts
@@ -8,4 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./notifications.page.css']
 })
 export class NotificationsPage {
+  notifications = [
+    {text : 'test1'},
+    {text : 'test2'},
+    {text : 'test3'},
+    {text : 'test4'},
+  ];
+
+
+  async getNotifcations(){
+    //send request to get notifications
+
+    //populate notifcations array
+  }
 }
+
+// {
+//     user_id: '1234',
+//     type: NotificationType.FollowNotification,
+//     seen: false,
+//     timestamp: Timestamp,
+//     notification_id: '1234',
+//     follower_id: '1234',
+// }
