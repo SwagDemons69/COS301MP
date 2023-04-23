0f8e8efefe37d04c69a8d5194e2dcee773d75887
Rec1dite <rec1dite@gmail.com>
2023-04-23 14:43:55 +0000

    Partially fixed post page styling

--- libs/app/post/feature/src/lib/post.page.ts
+++ libs/app/post/feature/src/lib/post.page.ts
@@ -1,18 +1,54 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 // import { Component } from '@angular/core';
 // import { NavController } from '@ionic/angular/providers/nav-controller';
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'post-page',
   templateUrl: './post.page.html',
-  styleUrls: ['./post.page.css']
+  styleUrls: ['./post.page.scss']
 })
 export class PostPage{
 
   constructor() {
-    
   }
- 
+
+  caption = ""
+  tags = [
+    "", "The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"
+  ]
+
+  updateTagList() {
+    const tagList = document.getElementById("tagList");
+    if(tagList == null) return;
+
+    // Remove previous tags
+    while(tagList.lastChild) {
+      document.removeChild(tagList.lastChild);
+    }
+
+    this.tags.forEach(x => x.toLowerCase().trim());
+    this.tags.sort();
+
+    let prevTag = "";
+    for(let t = 0; t < this.tags.length; t++) {
+      const tag = this.tags[t];
+
+      // Skip duplicates and empty strings
+      if(tag == prevTag) {
+        continue;
+      }
+
+      const el = document.createElement("ion-badge");
+      el.innerText = "#" + tag;
+
+      tagList?.appendChild(el);
+
+      prevTag = tag;
+    }
+  }
+
+  onCaptionChanged() {
+  }
 }
