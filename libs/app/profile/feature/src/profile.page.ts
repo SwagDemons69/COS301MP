37186a57d8bd808e7404e02de54569216b9b90cd
Mduduzi Sibiya <mdundoro@gmail.com>
2023-04-23 17:06:09 +0200

    I added a follow and message button  on the profile page
    The message button is also linked

--- libs/app/profile/feature/src/profile.page.ts
+++ libs/app/profile/feature/src/profile.page.ts
@@ -3,6 +3,7 @@ import { IProfile } from '@mp/api/profiles/util';
 import { ProfileState } from '@mp/app/profile/data-access';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'ms-profile-page',
@@ -11,4 +12,10 @@ import { Observable } from 'rxjs';
 })
 export class ProfilePage {
   @Select(ProfileState.profile) profile$!: Observable<IProfile | null>;
+
+  constructor(private navCtrl: NavController) { } // <-- inject NavController
+
+  goToMessages() {
+    this.navCtrl.navigateForward('/home/chat'); // <-- use NavController to navigate to messages page
+  }
 }
