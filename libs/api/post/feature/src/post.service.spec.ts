a460126c48de7f0bc1493f861f64cdde02864df5
gryffindor-coder <wiandtoit@gmail.com>
2023-04-25 17:09:52 +0200

    Added photo for testing in posts. Added first part of describe for testing

--- libs/api/post/feature/src/post.service.spec.ts
+++ libs/api/post/feature/src/post.service.spec.ts
@@ -1,2 +1,19 @@
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { PostService } from './post.service';
+import { AddPhotoRequest } from '../../util/src/requests';
+
+describe('Post feature', () => {
+    let postFeature: PostService;
+
+    beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+            providers: [PostService],
+        }).compile();
+
+        postFeature = moduleRef.get<PostService>(PostService);
+    });
+
+    it('should add a photo', async () => {
+        const addphotorequest = new AddPhotoRequest
+    })
+});
