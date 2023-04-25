4c8f6cbb1e2caf3930586af1f0c0dd12f7ea7d5a
gryffindor-coder <wiandtoit@gmail.com>
2023-04-25 19:14:53 +0200

    Added tester for AddPhoto endpoint

--- libs/api/post/feature/src/post.service.spec.ts
+++ libs/api/post/feature/src/post.service.spec.ts
@@ -1,19 +1,45 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { PostService } from './post.service';
 import { AddPhotoRequest } from '../../util/src/requests';
+import { CommandBus } from '@nestjs/cqrs';
+import { AddPhotoResponse } from '../../util/src/responses';
+import { AddPhotoCommand } from '../../util/src/commands';
 
 describe('Post feature', () => {
     let postFeature: PostService;
+    let commandBus: CommandBus;
 
     beforeEach(async () => {
         const moduleRef = await Test.createTestingModule({
-            providers: [PostService],
+            providers: [
+                PostService,
+            {
+                provide: CommandBus,
+                useValue: {
+                    execute: jest.fn(),
+                },
+            }],
         }).compile();
 
         postFeature = moduleRef.get<PostService>(PostService);
+        commandBus = moduleRef.get<CommandBus>(CommandBus);
     });
 
-    it('should add a photo', async () => {
-        const addphotorequest = new AddPhotoRequest
-    })
+    describe('addphoto', () => {
+        it('should add a photo by calling commandBus.execute with AddPhotoCommand', async () => {
+            const file = './testphoto.jpg';
+            const fileName = 'testphoto.jpg';
+            const request: AddPhotoRequest = {file, fileName};
+            const pathToImage = './testphoto.jpg';
+            const response: AddPhotoResponse = {pathToImage};
+            jest.spyOn(commandBus, 'execute').mockResolvedValue(response);
+
+            const result = await postFeature.AddPhoto(request);
+
+            expect(commandBus.execute).toHaveBeenCalledWith(new AddPhotoCommand(request));
+            expect(result).toEqual(response);
+        });
+    });
+
+    
 });
