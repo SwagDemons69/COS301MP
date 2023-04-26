ae4664a3e6f5d1c303e9f6a6e405a857ce9d01ae
Jake <jake.mileham@gmail.com>
2023-04-26 14:49:53 +0000

    Fixed tsconfig errors for profile-other and notifications.

--- libs/app/profile-other/data-access/jest.config.ts
+++ libs/app/profile-other/data-access/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'app-dashboard-data-access',
+  displayName: 'app-profile-other-data-access',
   preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../../coverage/libs/app/dashboard/data-access',
+  coverageDirectory: '../../../../coverage/libs/app/profile-other/data-access',
 };
